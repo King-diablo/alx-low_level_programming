9c84aa4f70ffa597cd5de26a7f874b61c375d45a
chikeluba <chikelubaobioraeze@gmail.com>
2022-10-14 08:22:05 -0700

    minor fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,9 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'e' || letter != 'q')
-			putchar (letter);
+		if (letter == 'e' || letter == 'q')
+			continue;
+		putchar (letter);
 	}
 	putchar ('\n');
 	return (0);
