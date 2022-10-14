3bd9ba9ac632608919748198dc84408235bb040e
chikeluba <chikelubaobioraeze@gmail.com>
2022-10-14 08:19:15 -0700

    final fix for file 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	char  letter;
-	
-	for(letter = 'a'; letter <= 'z'; letter++)
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if(letter != 'e' || letter != 'q')
-			putchar(letter);
+		if (letter != 'e' || letter != 'q')
+			putchar (letter);
 	}
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
