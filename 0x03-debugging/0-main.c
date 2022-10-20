e6d0acd90fcda3f08810f7e6210508f1db861443
Chikeluba Obiora-Eze <59526557+King-diablo@users.noreply.github.com>
2022-10-20 11:33:11 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,31 @@
 #include "main.h"
 
+
+
 /**
- * * main - this checks if the interger is positive or negative
+ *
+ * * main - tests function that prints if integer is positive or negative
  *
  * * Return: 0
  *
- **/
+ * */
+
+
 
 int main(void)
+
 {
-	int i;
 
-	i = 98;
+	        int i;
 
-	positive_or_negative(i);
-	return (0);
+
+
+		        i = 0;
+
+			        positive_or_negative(i);
+
+
+
+				        return (0);
+
 }
