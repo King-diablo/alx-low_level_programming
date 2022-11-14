845f1a93c07d476d1334f0291b6e794f23918b1e
chikeluba <chikelubaobioraeze@gmail.com>
2022-11-14 10:34:57 -0800

    Structs Task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *  * new_dog - create a new dog
+ *   * @name: char string name
+ *    * @age: int age
+ *     * @owner: char string owner
+ *      * Return: pointer to new dog
+ *       */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+		dog_t *dogN;
+			int i, j, k;
+				char *n, *o;
+
+					dogN = malloc(sizeof(dog_t));
+						if (dogN == NULL)
+									return (NULL);
+
+							for (i = 0; name[i] != '\0'; i++)
+										;
+								for (j = 0; owner[j] != '\0'; j++)
+											;
+
+									n = malloc(sizeof(char) * i + 1);
+										if (n == NULL)
+												{
+															free(dogN);
+																	return (NULL);
+																		}
+											o = malloc(sizeof(char) * j + 1);
+												if (o == NULL)
+														{
+																	free(n);
+																			free(dogN);
+																					return (NULL);
+																						}
+													for (k = 0; k <= i; k++)
+																n[k] = name[k];
+														for (k = 0; k <= j; k++)
+																	o[k] = owner[k];
+
+															dogN->name = n;
+																dogN->age = age;
+																	dogN->owner = o;
+
+																		return (dogN);
+}
